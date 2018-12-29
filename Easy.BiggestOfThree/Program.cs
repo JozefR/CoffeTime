cb2b9b1b216385b488b5c2fded496a1d61c54756
Jozef Randjak <randjakjozef@gmail.com>
2018-12-29 20:42:20 +0100

    add solution for biggestofthree

--- Easy.BiggestOfThree/Program.cs
+++ Easy.BiggestOfThree/Program.cs
@@ -6,7 +6,30 @@ namespace Easy.BiggestOfThree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sln = BiggestOfThree(3, 5, 7);
+            var sln2 = BiggestOfThree(4, 8, 6);
+        }
+
+        private static int BiggestOfThree(int A, int B, int C)
+        {
+            int biggest = 0;
+
+            if (A > B)
+            {
+                if (A > C)
+                    biggest = A;
+                else
+                    biggest = C;
+            }
+            else
+            {
+                if (B > C)
+                    biggest = B;
+                else
+                    biggest = C;
+            }
+
+            return biggest;
         }
     }
 }
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Some challenges from different sources with flowcharts.
 ## Easy
 
-####Biggest of three
+#### Biggest of three
 
 ![BiggestOfThreeFlowChart](https://github.com/JozefR/CoffeTime/blob/master/Easy.BiggestOfThree/BiggestOfThree.png)
