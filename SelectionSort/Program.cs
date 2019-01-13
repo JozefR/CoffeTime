84af711f31ac1462c7a7cad5a28ba6dab63834b2
Jozef Randjak <randjakjozef@gmail.com>
2019-01-13 11:05:17 +0100

    update selection sort algorithm

--- SelectionSort/Program.cs
+++ SelectionSort/Program.cs
@@ -7,7 +7,8 @@ namespace SelectionSort
         static void Main(string[] args)
         {
             int[] array = new[] {1, 3, 5, 2, 10, 6, 7};
-            var sortedArray = selectionSort(array);
+            //var sortedArray = selectionSort(array);
+            var sortedArray = selectionSortRefactored(array);
         }
 
         private static int[] selectionSort(int[] array)
@@ -32,6 +33,43 @@ namespace SelectionSort
             }
 
             return array;
+        }
+
+        private static int[] selectionSortRefactored(int[] array)
+        {
+            for (int firstIndex = 0; firstIndex < array.Length; firstIndex++)
+            {
+                int indexOfMinimum = IndexOfMinimum(array, firstIndex);                
+                Swap(array, firstIndex, indexOfMinimum);
+            }
+
+            return array;
+        }
+        
+        // This function will swap element in a given array.
+        private static void Swap(int[] array, int firstIndex, int indexOfMinimum)
+        {
+            var temp = array[firstIndex];
+            array[firstIndex] = array[indexOfMinimum];
+            array[indexOfMinimum] = temp;
+        }
+        
+        // This funcion will find index of minimum value
+        private static int IndexOfMinimum(int[] array, int startIndex)
+        {
+            var minimumIndex = startIndex;
+            var minimumValue = array[startIndex];
+            
+            for (int secondIndex = startIndex; secondIndex < array.Length; secondIndex++)
+            {
+                if (minimumValue > array[secondIndex])
+                {
+                    minimumIndex = secondIndex;
+                    minimumValue = array[secondIndex];
+                }
+            }
+
+            return minimumIndex;
         }
     }
 }
