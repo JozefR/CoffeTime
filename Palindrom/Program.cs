f01a8fc30975860374015a333db7c0afbc35a165
Jozef Randjak <randjakj@bluepartners.cz>
2019-04-19 09:13:53 +0200

    add palindrom recursion

---
+++ Palindrom/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nonRec = PalindromNonRec("rotor");
+            var rec = PalindromRecursion("rotor", 0);
+        }
+
+        private static bool PalindromRecursion(string rotor, int i)
+        {
+            if (rotor.Length <= 1)
+                return true;
+
+            if (i == rotor.Length - 1 - i)
+                return true;
+
+            if (rotor[i] != rotor[rotor.Length - i - 1])
+                return false;
+
+            return PalindromRecursion(rotor, i + 1);
+        }
+
+        private static bool PalindromNonRec(string rotor)
+        {
+            var result = "";
+
+            for (int i = rotor.Length - 1; i >= 0; i--)
+            {
+                result += rotor[i];
+            }
+
+            if (result == rotor)
+                return true;
+
+            return false;
+        }
+    }
+}
