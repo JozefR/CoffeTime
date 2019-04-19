c748be992f1da6f1c205a7a2414269230191eae6
Jozef Randjak <randjakj@bluepartners.cz>
2019-04-19 08:32:55 +0200

    add greatest common divisor

---
+++ GreatestCommonDivisor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GreatestCommonDivisor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gcdNon = GCDNonRecursion(24, 12);
+            var gcdNon2 = GCDNonRecursion(42, 56);
+            var gcd = GCD(24, 12);
+            var gcd2 = GCD(42, 56);
+        }
+
+        private static int GCDNonRecursion(int a, int b)
+        {
+            while (b != 0)
+            {
+                int rem = a % b;
+                a = b;
+                b = rem;
+            }
+
+            return a;
+        }
+
+        private static int GCD(int a, int b)
+        {
+            if (b == 0)
+                return a;
+
+            return GCD(b, a % b);
+        }
+    }
+}
