f064f705954ad69c0860c651bd1235fb39f7e23d
Jozef Randjak <randjakjozef@gmail.com>
2019-04-21 10:16:22 +0200

    fibonacci without recursion

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -6,12 +6,13 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             var fibo = FibonacciFunc(6);
             var fibo2 = FibonacciFunc(15);
 
             var dyna = FibonacciDynamic(6, new int [6]);
-            var dyna2 = FibonacciDynamic(15, new int [15]);
+            var  dyna2 = FibonacciDynamic(15, new int [15]);
+
+            var down = FibonacciFromDown(6);
         }
 
         private static int FibonacciFunc(int x)
@@ -43,7 +44,24 @@ namespace Fibonacci
                 helpArray[x - 2] = FibonacciDynamic(x - 2, helpArray);
             }
 
-            return helpArray[x - 2] + helpArray[x - 1];
+            return helpArray[x - 1] + helpArray[x - 2];
+        }
+
+        private static int FibonacciFromDown(int x)
+        {
+            var array = new int[x + 1];
+
+            array[0] = 0;
+            array[1] = 1;
+
+            for (int i = 2; i <= x; i++)
+            {
+                array[i] = array[i - 1] + array[i - 2];
+            }
+
+            var result = array[x];
+
+            return result;
         }
     }
 }
