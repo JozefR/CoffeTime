c332636bda439b0b05a78e6a6524ad9d10a308cb
Jozef Randjak <randjakjozef@gmail.com>
2019-06-08 14:39:38 +0200

    delete old recursion

---
+++ Algorithms/Recursion/Factorial/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Factorial
+ {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var recursion = FactorialFunc(3);
+            var nonRec  = WithoutRecursion(3);
+        }
+
+        #region factorial
+        private static int FactorialFunc(int i)
+        {
+            if (i == 1)
+                return 1;
+
+            return i * FactorialFunc(i - 1);
+        }
+
+        private static int WithoutRecursion(int number)
+        {
+            int result = number;
+
+            for (int i = number - 1; i > 0; i--)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+        #endregion
+    }
+}
