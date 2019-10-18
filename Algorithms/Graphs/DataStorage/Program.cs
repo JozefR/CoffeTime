90b29b981b211f4c7033c480d17e8619732136e3
JozefR <randjakjozef@gmail.com>
2019-10-18 17:49:49 +0200

    create matrix from linked list graph

--- Algorithms/Graphs/DataStorage/Program.cs
+++ Algorithms/Graphs/DataStorage/Program.cs
@@ -9,8 +9,18 @@ namespace DataStorage
         static void Main(string[] args)
         {
             var inputData = System.IO.File.ReadAllLines("../../../graphData.txt");
-            Graph graph = new Graph();
             
+            Graph graphEmpty = new Graph();
+
+            Graph graph = CreateLinkedGraph(inputData, graphEmpty);
+
+            int[,] matrix = CreateMatrix(graph);
+            
+            PrintMatrix(matrix);
+        }
+
+        private static Graph CreateLinkedGraph(string[] inputData, Graph graph)
+        {
             for (int i = 0; i < inputData.Length; i++)
             {
                 if (i == 0)
@@ -22,6 +32,44 @@ namespace DataStorage
                 var edge = inputData[i].Split(" ").Select(int.Parse).ToArray();
                 graph.AddEdge(edge[0], edge[0], edge[1]);
             }
+
+            return graph;
+        }
+
+        private static int[,] CreateMatrix(Graph graph)
+        {
+            // Create matrix based on graph vertexes length.
+            // set matrix based on vertex edges. 
+            int rows = graph.Vertices.Count;
+            int columns = graph.Vertices.Count;
+            
+            int[,] matrix = new int[rows, columns];
+
+            foreach (var vertice in graph.Vertices)
+            {
+                foreach (var edge in vertice.Edges)
+                {
+                    // because matrix is indexed from zero we need to decrease from, to edges
+                    int matrixColumn = edge.From - 1;
+                    int matrixRow = edge.To - 1;
+                    matrix[matrixRow, matrixColumn] = 1;
+                }
+            }
+
+            return matrix;
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(" " + matrix[i, j]);
+                }
+
+                Console.WriteLine("");
+            }                
         }
     }
 
