68dfe716bd5404e1cbe53a437644d9a3b3c72b96
Jozef Randjak <randjakjozef@gmail.com>
2021-10-26 14:47:37 +0200

    s

--- Challenges/EasyContainer/Program.cs
+++ Challenges/EasyContainer/Program.cs
@@ -6,7 +6,7 @@ namespace EasyContainer
     class TwoSum
     {
         /*
-         https://leetcode.com/problems/two-sum/
+        https://leetcode.com/problems/two-sum/
         Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
         You may assume that each input would have exactly one solution, and you may not use the same element twice.
         You can return the answer in any order.
@@ -35,7 +35,8 @@ namespace EasyContainer
         Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
         
         Complexity Analysis
-
+        Time complexity: O(n^2). For each element, we try to find its complement by looping through the rest of the array which takes O(n) time. Therefore, the time complexity is O(n^2).
+        Space complexity: O(1). The space required does not depend on the size of the input array, so only constant space is used.
          */
 
         public static int[] TwoSumBruteForce(int[] array, int target)
@@ -53,12 +54,20 @@ namespace EasyContainer
 
             return null;
         }
+        
         /*
-        Complexity Analysis
-        Time complexity: O(n^2). For each element, we try to find its complement by looping through the rest of the array which takes O(n) time. Therefore, the time complexity is O(n^2).
-        Space complexity: O(1). The space required does not depend on the size of the input array, so only constant space is used.
-         */
+        Approach 3: One-pass Hash Table
 
+        It turns out we can do it in one-pass. While we are iterating and inserting elements into the hash table, 
+        we also look back to check if current element's complement already exists in the hash table. 
+        If it exists, we have found a solution and return the indices immediately.
+
+        Complexity Analysis
+
+        Time complexity: O(n). We traverse the list containing n elements only once. Each lookup in the table costs only O(1) time.
+        Space complexity: O(n). The extra space required depends on the number of items stored in the hash table, which stores at most n elements.
+         */
+        
         public static int[] TwoSumDictionary(int[] array, int target)
         {
             var dictionary = new Dictionary<int, int>();
