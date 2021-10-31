77188dbcd9a17524549bc78904c80505028bc37f
Jozef Randjak <randjakjozef@gmail.com>
2021-10-31 14:55:28 +0100

    AddTwoNumber

--- Challenges/EasyContainer/Program.cs
+++ Challenges/EasyContainer/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
 using NUnit.Framework;
 
 namespace EasyContainer
@@ -101,6 +104,137 @@ namespace EasyContainer
                 Assert.AreEqual(new int[] { 0, 1 }, TwoSumDictionary(new []{ 2, 7, 11, 15 }, 9));
                 Assert.AreEqual(new int[] { 1, 2 }, TwoSumDictionary(new []{ 3, 2, 4 }, 6));
                 Assert.AreEqual(new int[] { 0, 1 }, TwoSumDictionary(new []{ 3 ,3 }, 6));
+            }
+        }
+    } 
+    
+    class AddTwoNumbersMedium
+    {
+        /*
+        You are given two non-empty linked lists representing two non-negative integers. 
+        The digits are stored in reverse order, and each of their nodes contains a single digit. 
+        Add the two numbers and return the sum as a linked list.
+        
+        Example 1:
+
+        2 -> 4 -> 3
+        5 -> 6 -> 4
+        -----------
+        7 -> 0 -> 8
+
+        You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+        
+        Input: l1 = [2,4,3], l2 = [5,6,4]
+        Output: [7,0,8]
+        Explanation: 342 + 465 = 807.
+        
+        Example 2:
+
+        Input: l1 = [0], l2 = [0]
+        Output: [0]
+        Example 3:
+
+        Example 3:
+        
+        Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+        Output: [8,9,9,9,0,0,0,1]
+        
+        Constraints:
+
+        The number of nodes in each linked list is in the range [1, 100].   
+        0 <= Node.val <= 9
+        It is guaranteed that the list represents a number that does not have leading zeros.
+         */
+
+        /**
+         * Definition for singly-linked list.
+         * public class ListNode {
+         *     public int val;
+         *     public ListNode next;
+         *     public ListNode(int val=0, ListNode next=null) {
+         *         this.val = val;
+         *         this.next = next;
+         *     }
+         * }
+         */
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        
+        // reverse the lists, also the result
+        // append the numbers to string
+        // sum both strings
+        // return result as a reversed linked list
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var l1String = string.Empty;
+            var iterate = l1;
+
+            while (iterate != null)
+            {
+                l1String += iterate.val;
+
+                iterate = iterate.next;
+            }
+            
+            char[] l1ReversedChar = l1String.ToCharArray();
+            Array.Reverse(l1ReversedChar);
+            string l1Reversed = new string(l1ReversedChar);
+
+            var l2string = string.Empty;
+            var iterate2 = l2;
+            
+            while (iterate2 != null)
+            {
+                l2string += iterate2.val;
+
+                iterate2 = iterate2.next;
+            }
+
+            char[] l2ReversedChar = l2string.ToCharArray();
+            Array.Reverse(l2ReversedChar);
+            string l2Reversed = new string(l2ReversedChar);
+
+            var sum = (BigInteger.Parse(l1Reversed.ToString()) + BigInteger.Parse(l2Reversed.ToString())).ToString().ToList();
+
+            ListNode temp = new ListNode(int.Parse(sum[0].ToString()), null);
+            ListNode result = null;
+            for (int i = 1; i < sum.Count; i++)
+            { 
+                var second = new ListNode(int.Parse(sum[i].ToString()), temp);
+                temp = second;
+            }
+
+            return temp;
+        }
+
+        [TestFixture]
+        public static class AddTwoNumbersTests
+        {
+            [Test]
+            public static void AreAddTwoNumbers()
+            {
+                var l13 = new ListNode(3, null);
+                var l12 = new ListNode(4, l13);
+                var l11 = new ListNode(2, l12);
+                
+                var l23 = new ListNode(4, null);
+                var l22 = new ListNode(6, l23);
+                var l21 = new ListNode(5, l22);     
+                
+                var l33 = new ListNode(8, null);
+                var l32 = new ListNode(0, l33);
+                var l31 = new ListNode(7, l32);
+                
+                Assert.AreEqual(l31, AddTwoNumbers(l11, l21));
             }
         }
     }
